c2a8dab38f820a2fd330ba3408c94cae5d2925ab
xuhaojun <xuhaojuntw@gmail.com>
2022-04-30 23:55:40 +0800

    feat: xxhanlp client add options

--- src/server/lib/xxhanlp/XxHanlpClient.ts
+++ src/server/lib/xxhanlp/XxHanlpClient.ts
@@ -18,9 +18,25 @@ export interface HanAnalysisResult {
   ner_msra: Array<Array<NerMsraEntity>>;
 }
 
+/**
+ * @see https://hanlp.hankcs.com/docs/annotations/ner/msra.html
+ */
+export enum NerMsraTag {
+  Person = "PERSON",
+  Location = "LOCATION",
+  Organization = "ORGANIZATION",
+  Date = "DATE",
+  Duration = "DURATION",
+  Time = "TIME",
+  Percent = "PERCENT",
+  Money = "MONEY",
+  Frequency = "FREQUENCY",
+  Integer = "INTEGER",
+}
+
 export interface NerMsraEntity {
   tok: string;
-  ner: string;
+  ner: NerMsraTag;
   start: number;
   end: number;
 }
@@ -28,21 +44,42 @@ export interface NerMsraEntity {
 const parseNerMsra = (ner: Array<string | number>): NerMsraEntity => {
   return {
     tok: ner[0] as string,
-    ner: ner[1] as string,
+    ner: ner[1] as NerMsraTag,
     start: ner[2] as number,
     end: ner[3] as number,
   };
 };
 
-const getCacheKey = (linesOrText: Array<string> | string): string => {
+const getCacheKey = (
+  linesOrText: Array<string> | string,
+  tasks?: Array<TaskName>
+): string => {
   const lines = _.castArray(linesOrText);
   const txt = lines.join("");
   const buffer: Buffer = LZUTF8.compress(txt, {
     inputEncoding: "String",
     outputEncoding: "Buffer",
   });
-  return crypto.createHash("sha256").update(buffer).digest("base64");
+  const hashedText = crypto
+    .createHash("sha256")
+    .update(buffer)
+    .digest("base64");
+  return `${hashedText}${tasks?.join(",")}`;
 };
+
+export enum TaskName {
+  SplitSentences = "split_sentences",
+  TokFine = "tok_fine",
+  NerMsra = "ner_msra",
+}
+
+interface XxHanlpClientOptions {
+  enableAxiosRateLimit?: boolean;
+  axiosRateLimitConfig?: {
+    maxRequests?: number;
+    perMilliseconds?: number;
+  };
+}
 
 export class XxHanlpClient {
   private axios: AxiosInstance;
@@ -50,13 +87,23 @@ export class XxHanlpClient {
     analysis: LRUCache<string, HanAnalysisResult>;
   };
 
-  constructor() {
-    this.axios = axiosRateLimit(
-      Axios.create({
-        baseURL: process.env.XXHANLP_API_URL || "http://localhost:8000",
-      }),
-      { maxRequests: 500, perMilliseconds: 1000 * 1 }
-    );
+  constructor(_opts?: XxHanlpClientOptions) {
+    const defaultOptions: XxHanlpClientOptions = {
+      enableAxiosRateLimit: true,
+      axiosRateLimitConfig: {
+        maxRequests: 500,
+        perMilliseconds: 1000 * 1,
+      },
+    };
+    const opts = _.defaultsDeep(_opts, defaultOptions);
+    const axios = Axios.create({
+      baseURL: process.env.XXHANLP_API_URL || "http://localhost:8000",
+    });
+    if (opts.enableAxiosRateLimit) {
+      this.axios = axiosRateLimit(axios, opts.axiosRateLimitConfig);
+    } else {
+      this.axios = axios;
+    }
     this.caches = {
       analysis: new LRUCache({
         max: 300,
@@ -79,27 +126,30 @@ export class XxHanlpClient {
   }
 
   public async analysis(
-    linesOrText: Array<string> | string
+    linesOrText: Array<string> | string,
+    tasks?: Array<TaskName>
   ): Promise<HanAnalysisResult> {
-    const cacheKey = getCacheKey(linesOrText);
+    const cacheKey = getCacheKey(linesOrText, tasks);
     const hit = this.caches.analysis.get(cacheKey);
     if (hit) {
       return hit;
     } else {
-      const result = await this._analysis(linesOrText);
+      const result = await this._analysis(linesOrText, tasks);
       this.caches.analysis.set(cacheKey, result);
       return result;
     }
   }
 
   protected async _analysis(
-    linesOrText: Array<string> | string
+    linesOrText: Array<string> | string,
+    tasks?: Array<TaskName>
   ): Promise<HanAnalysisResult> {
     const { axios } = this;
     const body: {
       lines?: Array<string>;
       text?: string;
-    } = {};
+      tasks?: Array<TaskName>;
+    } = { ...{ tasks } };
     if (_.isArray(linesOrText)) {
       body.lines = linesOrText;
     } else {
@@ -108,6 +158,7 @@ export class XxHanlpClient {
 
     const data = (await axios.post<HanAnalysisResultOri>("/analysis", body))
       .data;
+
     const omitField: Paths<HanAnalysisResultOri> = "ner_msra";
     let newNerMsra: Array<Array<NerMsraEntity>>;
     if (data.ner_msra) {
--- src/server/lib/xxhanlp/nest/xxhanlp.service.ts
+++ src/server/lib/xxhanlp/nest/xxhanlp.service.ts
@@ -1,16 +1,18 @@
 import { Injectable } from "@nestjs/common";
-import { HanAnalysisResult, XxHanlpClient } from "../XxHanlpClient";
+import { HanAnalysisResult, TaskName, XxHanlpClient } from "../XxHanlpClient";
 
 @Injectable()
 export class XxHanlpService implements Pick<XxHanlpClient, "analysis"> {
   private xxhanlpClient: XxHanlpClient;
+
   constructor() {
-    this.xxhanlpClient = new XxHanlpClient();
+    this.xxhanlpClient = new XxHanlpClient({ enableAxiosRateLimit: false });
   }
 
   public async analysis(
-    linesOrText: Array<string> | string
+    linesOrText: Array<string> | string,
+    tasks?: Array<TaskName>
   ): Promise<HanAnalysisResult> {
-    return this.xxhanlpClient.analysis(linesOrText);
+    return this.xxhanlpClient.analysis(linesOrText, tasks);
   }
 }
