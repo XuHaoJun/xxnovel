c8ad185f295ab2505ea32968b64af3cc47caade6
xuhaojun <xuhaojuntw@gmail.com>
2022-05-05 08:12:58 +0800

    feat: add bookhouse part1

---
+++ src/pages/bookhouse.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+import _ from "lodash";
+import RGL, { WidthProvider, Layout } from "react-grid-layout";
+import { Box, NoSsr, Container } from "@mui/material";
+import { useDebounce } from "ahooks";
+
+import("react-grid-layout/css/styles.css");
+import("react-resizable/css/styles.css");
+
+const ReactGridLayout = WidthProvider(RGL);
+
+const getDefaultProps = (): ReactGridLayout.ReactGridLayoutProps => ({
+  className: "layout",
+  cols: 12,
+  rowHeight: 30,
+  compactType: null,
+});
+
+const BookHousePage = () => {
+  const defaultProps = getDefaultProps();
+  const props = defaultProps;
+  const items = 3;
+  const generateLayout = () => {
+    const p = defaultProps;
+    return _.map(new Array(items), function (item, i) {
+      const y: number = _.result(p, "y") || Math.ceil(Math.random() * 4) + 1;
+      return {
+        x: (i * 2) % 12,
+        y: Math.floor(i / 6) * y,
+        w: 2,
+        h: y,
+        i: i.toString(),
+      };
+    });
+  };
+
+  const generateDOM = () => {
+    return _.map(_.range(items), function (i) {
+      return (
+        <Box component="div" key={i} sx={{ backgroundColor: "gray" }}>
+          <span className="text">{i}</span>
+        </Box>
+      );
+    });
+  };
+
+  const [layout, setLayout] = React.useState(() => generateLayout());
+  const debounedLayout = useDebounce(layout, { wait: 3000 });
+
+  return (
+    <Container>
+      <NoSsr>
+        <ReactGridLayout
+          layout={debounedLayout}
+          onLayoutChange={(layout: Layout[]) => {
+            console.log(layout);
+            setLayout(layout);
+          }}
+          {...props}
+        >
+          {generateDOM()}
+        </ReactGridLayout>
+      </NoSsr>
+    </Container>
+  );
+};
+
+export default BookHousePage;
